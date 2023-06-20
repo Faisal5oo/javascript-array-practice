b739e11b845353c6d2101562f7687b3bfcbf0715
Faisal5oo <133655119+Faisal5oo@users.noreply.github.com>
2023-06-20 19:17:20 +0500

    array methods (#1)

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-console.log("hello world");
+const {
+  arr_at,
+  push_arr,
+  popOut,
+  shifted,
+  unshifted,
+  joined,
+  sliced,
+  spliced,
+  for_Each,
+  mapping,
+  evenNumber,
+  joining,
+  filling,
+  reversed,
+  reducing,
+} = require("./Arrays");
+// at()
+console.log(arr_at([1, 2, 3, 4, 5]));
+
+// push()
+let arr3 = [8, 9, 10];
+let arr1 = ["apple", "mango", "banana", "carrot", "lettuce", "onion"];
+push_arr(arr1);
+console.log(arr1);
+
+//pop()
+
+popOut(arr1);
+console.log(arr1);
+
+// shifted
+let arr2 = [1, 2, 3, 4, 5, 6];
+shifted(arr2);
+console.log(arr2);
+
+//unshift()
+unshifted(arr2);
+console.log(arr2);
+
+// joined()
+
+let combined = joined(arr2, arr3);
+console.log(combined);
+
+// slice()
+
+let slicedValue = sliced(arr2);
+console.log(slicedValue);
+// spliced()
+
+spliced(arr1);
+console.log(arr1);
+
+// foreach()
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+for_Each(numbers);
+
+// map()
+let square = mapping(arr2);
+console.log(square);
+
+// filter()
+let filtered = evenNumber(arr2);
+console.log(filtered);
+//join()
+let join_arr = joining(arr2);
+console.log(join_arr);
+
+//fill()
+
+let filled = filling(arr2);
+console.log(filled);
+
+// reversed()
+
+let changedValue = reversed(arr3);
+console.log(changedValue);
+
+// reduce()
+let compressedValue = reducing(arr3);
+console.log(compressedValue);
